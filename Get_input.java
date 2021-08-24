709b07b45e6b70331bb486189fb3a172f556f60a
Yotsarawoot <6330613016@email.psu.ac.th>
2021-08-24 09:25:59 +0700

    Commit Scanner test

---
+++ Get_input.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+class GetInputFromUser
+{
+    public static void main (String args[])
+    {
+        Scanner in = new Scanner(System.in);
+        int a = in.nextInt();
+        System.out.println("Your entered integer "+a);
+        float b = in.nextFloat();
+        System.out.println("Your entered float "+b);
+    }
+}
+
