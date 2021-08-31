40acc8c10d57493c8910b4399f6ce2e2ce482fde
Yotsarawoot <6330613016@email.psu.ac.th>
2021-08-31 10:16:30 +0700

    Commit Q4

---
+++ Lab2/Q4.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class Q4 {
+    
+    public static void main(String[] args)
+    {
+        Scanner in = new Scanner(System.in);
+        int Integer = in.nextInt();
+        int Sum = evenSum1toN(Integer);
+        System.out.println(Sum);
+    }
+
+    public static int evenSum1toN (int Integer)
+    {
+        int Sum = 0;
+        for(int i = 1; i <= Integer; i++)
+        {
+            if( i % 2 == 0 ) Sum = Sum + i;
+        }
+        return Sum;
+    }
+}
