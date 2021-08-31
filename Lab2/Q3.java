c0f0bfd76f2d74a640453115bd9d97cb775507ef
Yotsarawoot <6330613016@email.psu.ac.th>
2021-08-31 09:02:49 +0700

    Commit Q3

---
+++ Lab2/Q3.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class Q3 
+{
+    
+    public static void main (String[] args)
+    {
+        Scanner in = new Scanner(System.in);
+        int Celsius = in.nextInt();
+        printWeather(Celsius);
+    }
+
+    public static void printWeather (int Celsius)
+    {
+        if(Celsius < 0) System.out.println("Freezing Weather");
+        else if(Celsius >= 0 && Celsius < 10) System.out.println("Very Cold Weather");
+        else if(Celsius >= 10 && Celsius < 20) System.out.println("Cold Weather");
+        else if(Celsius >= 20 && Celsius < 30) System.out.println("Normal in Temp");
+        else if(Celsius >= 30 && Celsius < 40) System.out.println("It's hot");
+        else System.out.println("It's very hot");
+    }
+}
