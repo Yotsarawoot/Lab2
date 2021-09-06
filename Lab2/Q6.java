6cca3b8bec683bf7ae701a1b633b29d4f49811aa
Yotsarawoot <6330613016@email.psu.ac.th>
2021-09-07 02:22:11 +0700

    Commit Q6

---
+++ Lab2/Q6.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+
+public class Q6 {
+
+    public static void main(String[] args)
+    {
+        Scanner in = new Scanner(System.in);
+        int Charge = in.nextInt();
+        double Cost = 0;
+        if(Charge > 0 && Charge <= 35)
+        {
+            Cost = 85;
+        }
+        else if(Charge > 35 && Charge <= 150)
+        {
+            Cost = 85 + 1.12*(Charge - 35);
+        }
+        else if(Charge > 150)
+        {
+            Cost = 85 + 1.12*(115) + + 2.13*(Charge - 150);
+        }
+        else
+        {
+
+        }
+        System.out.print(Cost);
+    }
+    
+}
